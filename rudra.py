65b5781c2c320e456b5df0dba576abc6469abc02
Bittu2217 <payslice604@gmail.com>
2024-08-18 00:03:51 +0530

    Update rudra.py

--- rudra.py
+++ rudra.py
@@ -16,11 +16,11 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = '7499514091:AAGnkWVoHijJIJtAa0FwylHQQgR8BsXdZ14'
+TOKEN = '7508241958:AAFNvdYEchufmqPbDZkuPgwv0vqsUQZ7c-E'
 MONGO_URI = 'mongodb+srv://piroop:piroop@piro.hexrg9w.mongodb.net/?retryWrites=true&w=majority&appName=piro&tlsAllowInvalidCertificates=true'
-FORWARD_CHANNEL_ID = -1002192880793
-CHANNEL_ID = -1002192880793
-error_channel_id = -1002192880793
+FORWARD_CHANNEL_ID = -6595944212
+CHANNEL_ID = -6595944212
+error_channel_id = -6595944212
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
